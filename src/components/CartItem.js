781f0d876956c308c91af447db73717d24f7d3a4
CodyKoInABox <codykoinabox@gmail.com>
2025-03-21 00:52:33 -0300

    create cart item component

---
+++ src/components/CartItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const CartItem = ({ item, updateQuantity, removeFromCart }) => {
+  return (
+    <div className="cart-item" data-cy="cart-item">
+      <img 
+        src={item.image} 
+        alt={item.name} 
+        className="cart-item-image"
+      />
+      <div className="cart-item-details">
+        <h4 className="cart-item-title" data-cy="cart-item-name">{item.name}</h4>
+        <p className="cart-item-price" data-cy="cart-item-price">${item.price.toFixed(2)}</p>
+        <div className="cart-item-actions">
+          <button 
+            className="qty-btn"
+            onClick={() => updateQuantity(item.id, item.quantity - 1)}
+            disabled={item.quantity <= 1}
+            data-cy="decrease-quantity"
+          >
+            -
+          </button>
+          <span className="cart-quantity" data-cy="item-quantity">{item.quantity}</span>
+          <button 
+            className="qty-btn"
+            onClick={() => updateQuantity(item.id, item.quantity + 1)}
+            data-cy="increase-quantity"
+          >
+            +
+          </button>
+          <button 
+            className="remove-btn"
+            onClick={() => removeFromCart(item.id)}
+            data-cy="remove-item"
+          >
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
