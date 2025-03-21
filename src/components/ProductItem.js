1e19f659925aa050a5691fcc196ba4c4080de3a6
CodyKoInABox <codykoinabox@gmail.com>
2025-03-21 00:52:06 -0300

    create product item component

---
+++ src/components/ProductItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const ProductItem = ({ product, addToCart }) => {
+  return (
+    <div className="product-card" data-cy="product-item">
+      <img 
+        src={product.image} 
+        alt={product.name} 
+        className="product-image"
+        data-cy="product-image"
+      />
+      <h3 className="product-title" data-cy="product-name">{product.name}</h3>
+      <p className="product-price" data-cy="product-price">${product.price.toFixed(2)}</p>
+      <p>{product.description}</p>
+      <button 
+        className="add-to-cart-btn" 
+        onClick={() => addToCart(product)}
+        data-cy="add-to-cart-button"
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default ProductItem;
