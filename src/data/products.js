c00acf45ee6aa7132a39674e2dd6d1b2406d7201
CodyKoInABox <codykoinabox@gmail.com>
2025-03-21 01:10:30 -0300

    fix typos and images

--- src/data/products.js
+++ src/data/products.js
@@ -2,45 +2,45 @@
 const produtos = [
     {
       id: 1,
-      nome: "Árvore Cipreste Italiano",
-      preco: 59.99,
-      imagem: "https://cdn.mos.cms.futurecdn.net/Gvq3eSwgxy8L7EZ4nDH7KG.jpg",
-      descricao: "Árvore perene alta e colunar, perfeita para criar telas de privacidade elegantes"
+      name: "Árvore Cipreste Italiano",
+      price: 59.99,
+      image: "https://cdn.mos.cms.futurecdn.net/Gvq3eSwgxy8L7EZ4nDH7KG.jpg",
+      description: "Árvore perene alta e colunar, perfeita para criar telas de privacidade elegantes"
     },
     {
       id: 2,
-      nome: "Árvore Cipreste de Leyland",
-      preco: 49.99,
-      imagem: "https://https://eureka-farms.com/cdn/shop/files/Leyland_Cypress_SSsq.jpg?v=1735791751&width=3840",
-      descricao: "Cipreste híbrido de crescimento rápido, ideal para quebra-ventos e cercas vivas"
+      name: "Árvore Cipreste de Leyland",
+      price: 49.99,
+      image: "https://familiabim.com.br/image/cache/catalog/colaborador/Leyland%20Cypress-1000x1000.jpg",
+      description: "Cipreste híbrido de crescimento rápido, ideal para quebra-ventos e cercas vivas"
     },
     {
       id: 3,
-      nome: "Árvore Cipreste Calvo",
-      preco: 79.99,
-      imagem: "https://shop-static.arborday.org/media/0005495_baldcypress_510.jpeg",
-      descricao: "Conífera decídua distintiva que prospera em condições úmidas"
+      name: "Árvore Cipreste Calvo",
+      price: 79.99,
+      image: "https://shop-static.arborday.org/media/0005495_baldcypress_510.jpeg",
+      description: "Conífera decídua distintiva que prospera em condições úmidas"
     },
     {
       id: 4,
-      nome: "Cypress Hill: Black Sunday",
-      preco: 24.99,
-      imagem: "https://upload.wikimedia.org/wikipedia/pt/d/dd/Cypress_Hill_-_Black_Sunday.jpg",
-      descricao: "Álbum clássico de hip-hop de 1993 com a música 'Insane in the Brain'"
+      name: "Cypress Hill: Black Sunday",
+      price: 24.99,
+      image: "https://upload.wikimedia.org/wikipedia/pt/d/dd/Cypress_Hill_-_Black_Sunday.jpg",
+      description: "Álbum clássico de hip-hop de 1993 com a música 'Insane in the Brain'"
     },
     {
       id: 5,
-      nome: "Cypress Hill: Temples of Boom",
-      preco: 22.99,
-      imagem: "https://upload.wikimedia.org/wikipedia/pt/e/ef/III_Temples_of_Boom.jpg",
-      descricao: "Álbum psicodélico e sombrio de 1995 com produção hipnotizante"
+      name: "Cypress Hill: Temples of Boom",
+      price: 22.99,
+      image: "https://upload.wikimedia.org/wikipedia/pt/e/ef/III_Temples_of_Boom.jpg",
+      description: "Álbum psicodélico e sombrio de 1995 com produção hipnotizante"
     },
     {
       id: 6,
-      nome: "Cypress Hill: Self-Titled",
-      preco: 19.99,
-      imagem: "https://upload.wikimedia.org/wikipedia/en/8/85/Cypress_Hill_Self-Titled.jpg",
-      descricao: "Álbum de estreia revolucionário de 1991 que estabeleceu seu som único"
+      name: "Cypress Hill: Self-Titled",
+      price: 19.99,
+      image: "https://upload.wikimedia.org/wikipedia/en/8/85/Cypress_Hill_Self-Titled.jpg",
+      description: "Álbum de estreia revolucionário de 1991 que estabeleceu seu som único"
     }
   ];
   export default produtos;
