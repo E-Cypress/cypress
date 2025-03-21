c4fb40ace73389af7204fda1a9d8b975aca20bd3
CodyKoInABox <codykoinabox@gmail.com>
2025-03-21 01:15:47 -0300

    fix text ui bug

--- src/data/products.js
+++ src/data/products.js
@@ -2,45 +2,45 @@
 const produtos = [
     {
       id: 1,
-      name: "Árvore Cypress Italiano",
+      name: "Arvore Cypress Italiano",
       price: 59.99,
       image: "https://cdn.mos.cms.futurecdn.net/Gvq3eSwgxy8L7EZ4nDH7KG.jpg",
-      description: "Árvore perene alta e colunar, perfeita para criar telas de privacidade elegantes"
+      description: "Arvore perene alta e colunar, perfeita para criar telas de privacidade elegantes"
     },
     {
       id: 2,
-      name: "Árvore Cypress de Leyland",
+      name: "Arvore Cypress de Leyland",
       price: 49.99,
       image: "https://familiabim.com.br/image/cache/catalog/colaborador/Leyland%20Cypress-1000x1000.jpg",
-      description: "Cipreste híbrido de crescimento rápido, ideal para quebra-ventos e cercas vivas"
+      description: "Cipreste hibrido de crescimento rapido, ideal para quebra-ventos e cercas vivas"
     },
     {
       id: 3,
-      name: "Árvore Cypress Calvo",
+      name: "Arvore Cypress Calvo",
       price: 79.99,
       image: "https://shop-static.arborday.org/media/0005495_baldcypress_510.jpeg",
-      description: "Conífera decídua distintiva que prospera em condições úmidas"
+      description: "Conifera decidua distintiva que prospera em condicoes umidas"
     },
     {
       id: 4,
       name: "Cypress Hill: Black Sunday",
       price: 24.99,
       image: "https://upload.wikimedia.org/wikipedia/pt/d/dd/Cypress_Hill_-_Black_Sunday.jpg",
-      description: "Álbum clássico de hip-hop de 1993 com a música 'Insane in the Brain'"
+      description: "Album classico de hip-hop de 1993 com a musica 'Insane in the Brain'"
     },
     {
       id: 5,
       name: "Cypress Hill: Temples of Boom",
       price: 22.99,
       image: "https://upload.wikimedia.org/wikipedia/pt/e/ef/III_Temples_of_Boom.jpg",
-      description: "Álbum psicodélico e sombrio de 1995 com produção hipnotizante"
+      description: "Album psicodelico e sombrio de 1995 com producao hipnotizante"
     },
     {
       id: 6,
       name: "Cypress Hill: Self-Titled",
       price: 19.99,
       image: "https://upload.wikimedia.org/wikipedia/en/8/85/Cypress_Hill_Self-Titled.jpg",
-      description: "Álbum de estreia revolucionário de 1991 que estabeleceu seu som único"
+      description: "Album de estreia revolucionario de 1991 que estabeleceu seu som unico"
     }
   ];
   export default produtos;
